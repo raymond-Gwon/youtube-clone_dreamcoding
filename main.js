a95cc20e20ab85f02fe521019a5674a0d72e7cb4
raymond-Gwon <73869660+raymond-Gwon@users.noreply.github.com>
2020-12-02 06:32:14 +0900

    Cloning Youtube

---
+++ main.js
@@ -0,0 +1,7 @@
+const moreBtn = document.querySelector('.infoAndUpNext .info .metadata .titleAndButton .moreBtn');
+const title = document.querySelector('.infoAndUpNext .info .metadata .titleAndButton .title');
+
+moreBtn.addEventListener('click', () => {
+    moreBtn.classList.toggle('clicked');
+    title.classList.toggle('clamp');
+})
